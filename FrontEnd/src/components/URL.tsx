04ee0d2bcaaa3f38efb62ff936244d3f823cc7d9
Tigo-cmd <ja16162040@gmail.com>
2026-02-23 18:23:05 +0100

    chore: Update default prediction and chat URLs.

--- FrontEnd/src/components/URL.tsx
+++ FrontEnd/src/components/URL.tsx
@@ -1,2 +1,7 @@
-export const Default_Url_predict = "https://cnnclassifierapiserver-production.up.railway.app//predict";
-export const Default_Url_chat = "https://cnnclassifierapiserver-production.up.railway.app//chat";
+// export const Default_Url_predict = "https://cnnclassifierapiserver-production.up.railway.app//predict";
+// export const Default_Url_chat = "https://cnnclassifierapiserver-production.up.railway.app//chat";
+
+
+
+export const Default_Url_predict = "https://optimal-hebrew-properly-dedicated.trycloudflare.com/predict";
+export const Default_Url_chat = "https://optimal-hebrew-properly-dedicated.trycloudflare.com/chat";
