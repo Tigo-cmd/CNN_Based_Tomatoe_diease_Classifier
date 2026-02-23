ad8f3186266afc0ef3366ab69e47167ddb7c3c36
Tigo-cmd <ja16162040@gmail.com>
2026-02-23 22:01:32 +0100

    feat: Introduce and utilize  to simplify project-course assignment queries across the application.

--- FrontEnd/src/components/URL.tsx
+++ FrontEnd/src/components/URL.tsx
@@ -3,5 +3,5 @@
 
 
 
-export const Default_Url_predict = "https://https://bennett-blue-deer-preference.trycloudflare.com/predict";
-export const Default_Url_chat = "https://https://bennett-blue-deer-preference.trycloudflare.com/chat";
+export const Default_Url_predict = "https://bennett-blue-deer-preference.trycloudflare.com/predict";
+export const Default_Url_chat = "https://bennett-blue-deer-preference.trycloudflare.com/chat";
